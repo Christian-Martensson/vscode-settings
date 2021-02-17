37deb59e450cb957438f2dcc42ce5de86d7cd129
Christian Mårtensson <chris.martensson@protonmail.com>
2021-02-17 13:16:32 +0100

    added files

---
+++ globalStorage/iocave.monkey-patch/modules/main.js
@@ -0,0 +1,7 @@
+require.config({
+    paths: {
+        "monkey-generated" : "/Users/chris/Library/Application Support/Code/User/globalStorage/iocave.monkey-patch/modules"
+    }
+});
+
+define(["monkey-generated/entrypoint-main"], function (){});
